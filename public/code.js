ea3dd3adcf060731a119570a71df58b8dc4c3034
Niraj kumar chaurasiya <87218979+nirajkumar1234@users.noreply.github.com>
2023-05-15 12:04:26 +0545

    Add files via upload

---
+++ public/code.js
@@ -0,0 +1,88 @@
+(function () {
+    const app = document.querySelector(".app")
+    const socket = io();
+    let uname;
+    app.querySelector("#join-user").addEventListener("click", function () {
+        let username = app.querySelector("#username").value;
+        if (username.length == 0) {
+            return;
+        }
+
+        socket.emit("newuser", username);
+        uname = username;
+
+        app.querySelector(".join-screen").classList.remove("active")
+        app.querySelector(".chat-screen").classList.add("active")
+    })
+
+
+    app.querySelector("#send-message").addEventListener("click", function () {
+        let message = app.querySelector("#message-input").value;
+
+        if (message.length == 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        })
+        socket.emit("chat", {
+            username: uname,
+            text: message
+        })
+        app.querySelector("#message-input").value = "";
+    })
+
+    app.querySelector('#exit-chat').addEventListener("click", function () {
+        socket.emit("exituser", uname)
+        window.location.href = window.location.href;
+    })
+
+    socket.on("update", function (update) {
+        renderMessage("update", update)
+    })
+
+    socket.on("chat", function (message) {
+        renderMessage("other", message);
+    })
+
+    function renderMessage(type, message) {
+        let messageContainer = app.querySelector(".messages")
+        if (type == "my") {
+            let el = document.createElement("div")
+            el.setAttribute("class", "message my-message")
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+
+            messageContainer.appendChild(el);
+        }
+        else if (type == "other") {
+            let el = document.createElement("div")
+            el.setAttribute("class", "message other-message")
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+
+            messageContainer.appendChild(el);
+        }
+
+        else if (type == "update") {
+            let el = document.createElement("div")
+            el.setAttribute("class", "update")
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+
+    }
+})();
