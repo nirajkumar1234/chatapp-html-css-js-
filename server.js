2a6b5a7601b9b415d463160c607b2a39926a7341
Niraj kumar chaurasiya <87218979+nirajkumar1234@users.noreply.github.com>
2023-05-16 09:09:27 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.static(path.join(__dirname + "/public")));
 
 io.on("connection", function (socket) {
     socket.on("newuser", function (username) {
-        socket.broadcast.emit("update", username + " join the conversation")
-    })
+        socket.broadcast.emit("update", username + " joined the conversation");
+    });
+
     socket.on("exituser", function (username) {
-        socket.broadcast.emit("update", username + " exit the conversation")
-    })
+        socket.broadcast.emit("update", username + " exit the conversation");
+    });
+
+    let typingUsers = new Set();
+
+    socket.on("typing", function (username) {
+        typingUsers.add(username);
+        socket.broadcast.emit("typing", Array.from(typingUsers));
+    });
+
+    socket.on("stop-typing", function (username) {
+        typingUsers.delete(username);
+        socket.broadcast.emit("stop-typing", Array.from(typingUsers));
+    });
 
     socket.on("chat", function (message) {
-        socket.broadcast.emit("chat", message)
-    })
-})
+        socket.broadcast.emit("chat", message);
+        typingUsers.clear(); // clear typing users when message is sent
+    });
 
-
+});
 
 server.listen(5000, () => {
-    console.log(`App is listening on http://localhost:5000`)
-})
+    console.log(`App is listening on http://localhost:5000`);
+});
